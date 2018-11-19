041d2e7e34307c6ec74e32b23b0c2fc1827cbf83
JuanJo Nieto <juanjo.3ns@gmail.com>
2018-11-19 16:35:07 +0000

    3 dataloaders ready

--- src/dataloader/datasetLEDOV.py
+++ src/dataloader/datasetLEDOV.py
@@ -11,42 +11,53 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 
-# constants
-PATH_SALICON = "/media/eHD/juanjo/DATASETS/SALICON/"
+PATH = "/home/dataset/"
+TRAIN = 'train'
+VAL = 'val'
+numTraining = 456
+numValidation = 41
 
-
-class Salicon(Dataset):
-    def __init__(self, mode='train', transformation=False, return_path=False, N=None):
-        global PATH_SALICON
+class LEDOV(Dataset):
+    def __init__(self, mode='train',transformation=False, return_path=False, N=None):
+        global PATH_DHF1K
         self.size = (192, 256)
         self.mean = [103.939, 116.779, 123.68]
-        self.path_dataset = PATH_SALICON
-        self.path_images = os.path.join(self.path_dataset, 'images')
-        self.path_saliency = os.path.join(self.path_dataset, 'maps')
+        self.path_dataset = PATH
+        self.path_images = os.path.join(self.path_dataset, 'ledov_frames')
+        self.path_saliency = os.path.join(self.path_dataset, 'ledov_gt')
+
         self.transformation = transformation
         self.return_path = return_path
+		videos = os.listdir(self.path_images)
 
-        # get list images
-        list_names = os.listdir( os.path.join(self.path_dataset, 'fixations', mode) )
+        list_names = []
+        if mode == TRAIN:
+            for v in videos[:numTraining]:
+                for t in os.listdir(os.path.join(self.path_images, v)):
+                    list_names.append(v+t)
+        elif mode == VAL:
+            for v in videos[numTraining:(numTraining+numValidation)]:
+                for t in os.listdir(os.path.join(self.path_images, v)):
+                    list_names.append(v+t)
         list_names = np.array([n.split('.')[0] for n in list_names])
         self.list_names = list_names
 
         if N is not None:
             self.list_names = list_names[:N]
-
-        print("Init dataset in mode {}".format(mode))
-        print("\t total of {} images.".format(list_names.shape[0]))
+        print("Total of {} images.".format(self.list_names.shape[0]))
 
     def __len__(self):
         return self.list_names.shape[0]
 
     def __getitem__(self, index):
         # set path
-        ima_name = self.list_names[index]+'.jpg'
-        rgb_ima = os.path.join(self.path_images, ima_name)
+        ima_name = self.list_names[index][-4:]+'.jpg' # the last 4 numbers belong to the image name
+        vid = self.list_names[index][:len(self.list_names[index])-4] #  the others belong to the name of video
+
+        rgb_ima = os.path.join(self.path_images,vid, ima_name)
         image = cv2.imread(rgb_ima)
-        ima_name = self.list_names[index]+'.png'
-        saliency = cv2.imread(os.path.join(self.path_saliency, ima_name), 0)
+        ima_name = self.list_names[index][-4:]+'.png'
+        saliency = cv2.imread(os.path.join(self.path_saliency, vid,'GT','map', ima_name), 0)
 
         # apply transformation
         if self.transformation is not None:
---
+++ src/salgan_salicon/main.py
@@ -0,0 +1,75 @@
+import sys
+import torch
+from ..utils.salgan_generator import create_model
+from ..utils.salgan_utils import load_image, postprocess_prediction
+from ..utils.salgan_utils import normalize_map
+
+import numpy as np
+import os
+import cv2
+
+import matplotlib.pylab as plt
+from IPython import embed
+
+PATH_PYTORCH_WEIGHTS = 'model_weights/gen_model.pt'
+INPUT_PATH = '/salgan_pytorch/data/dhf1k_frames/'
+OUTPUT_PATH = '/salgan_pytorch/data/salganbaseline/'
+
+USE_GPU=True
+
+
+def main():
+	"""
+	Runs pytorch-SalGAN on a sample images
+
+	"""
+	# create output file
+	if not os.path.exists(OUTPUT_PATH):
+		os.makedirs(OUTPUT_PATH)
+
+	# init model with pre-trained weights
+	model = salgan_generator.create_model()
+
+	model.load_state_dict(torch.load(PATH_PYTORCH_WEIGHTS))
+	model.eval()
+
+
+	# if GPU is enabled
+	if USE_GPU:
+		model.cuda()
+
+	# load and preprocess images in folder
+	for y in range(601,701):
+		if not os.path.exists(os.path.join(OUTPUT_PATH,str(y))):
+			os.makedirs(os.path.join(OUTPUT_PATH,str(y)))
+		for i, name in enumerate(os.listdir(os.path.join(INPUT_PATH,'{:03d}'.format(y)))):
+			filename = os.path.join(INPUT_PATH,'{:03d}'.format(y), name)
+			image_tensor, image_size = load_image(filename)
+
+			if USE_GPU:
+				image_tensor = image_tensor.cuda()
+
+			# run model inference
+			prediction = model.forward(image_tensor[None, ...]) # add extra batch dimension
+
+			# get result to cpu and squeeze dimensions
+			if USE_GPU:
+				prediction = prediction.squeeze().data.cpu().numpy()
+			else:
+				prediction = prediction.squeeze().data.numpy()
+
+			# postprocess
+			# first normalize [0,1]
+			prediction = normalize_map(prediction)
+			saliency = postprocess_prediction(prediction, image_size)
+			saliency = normalize_map(saliency)
+			saliency *= 255
+			saliency = saliency.astype(np.uint8)
+			# save saliency
+
+			cv2.imwrite(os.path.join(OUTPUT_PATH,str(y),name), saliency)
+			print("Processed image {} from video {}".format(i,y), end="\r")
+			sys.stdout.flush()
+
+if __name__ == '__main__':
+	main()
