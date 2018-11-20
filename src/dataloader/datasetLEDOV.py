cad2c6b683cb7a6458e9ac435648bc2438b0c26f
enric <enric.moreu.filella@gmail.com>
2018-11-20 10:52:26 +0000

    salgan_ledov model ready for training

--- src/dataloader/datasetLEDOV.py
+++ src/dataloader/datasetLEDOV.py
@@ -18,68 +18,68 @@ numTraining = 456
 numValidation = 41
 
 class LEDOV(Dataset):
-    def __init__(self, mode='train',transformation=False, return_path=False, N=None):
-        global PATH_DHF1K
-        self.size = (192, 256)
-        self.mean = [103.939, 116.779, 123.68]
-        self.path_dataset = PATH
-        self.path_images = os.path.join(self.path_dataset, 'ledov_frames')
-        self.path_saliency = os.path.join(self.path_dataset, 'ledov_gt')
+	def __init__(self, mode='train',transformation=False, return_path=False, N=None):
+		global PATH_DHF1K
+		self.size = (192, 256)
+		self.mean = [103.939, 116.779, 123.68]
+		self.path_dataset = PATH
+		self.path_images = os.path.join(self.path_dataset, 'ledov_frames')
+		self.path_saliency = os.path.join(self.path_dataset, 'ledov_gt')
 
-        self.transformation = transformation
-        self.return_path = return_path
+		self.transformation = transformation
+		self.return_path = return_path
 		videos = os.listdir(self.path_images)
 
-        list_names = []
-        if mode == TRAIN:
-            for v in videos[:numTraining]:
-                for t in os.listdir(os.path.join(self.path_images, v)):
-                    list_names.append(v+t)
-        elif mode == VAL:
-            for v in videos[numTraining:(numTraining+numValidation)]:
-                for t in os.listdir(os.path.join(self.path_images, v)):
-                    list_names.append(v+t)
-        list_names = np.array([n.split('.')[0] for n in list_names])
-        self.list_names = list_names
+		list_names = []
+		if mode == TRAIN:
+			for v in videos[:numTraining]:
+				for t in os.listdir(os.path.join(self.path_images, v)):
+					list_names.append(v+t)
+		elif mode == VAL:
+			for v in videos[numTraining:(numTraining+numValidation)]:
+				for t in os.listdir(os.path.join(self.path_images, v)):
+					list_names.append(v+t)
+		list_names = np.array([n.split('.')[0] for n in list_names])
+		self.list_names = list_names
 
-        if N is not None:
-            self.list_names = list_names[:N]
-        print("Total of {} images.".format(self.list_names.shape[0]))
+		if N is not None:
+			self.list_names = list_names[:N]
+		print("Total of {} images.".format(self.list_names.shape[0]))
 
-    def __len__(self):
-        return self.list_names.shape[0]
+	def __len__(self):
+		return self.list_names.shape[0]
 
-    def __getitem__(self, index):
-        # set path
-        ima_name = self.list_names[index][-4:]+'.jpg' # the last 4 numbers belong to the image name
-        vid = self.list_names[index][:len(self.list_names[index])-4] #  the others belong to the name of video
+	def __getitem__(self, index):
+		# set path
+		ima_name = self.list_names[index][-4:]+'.jpg' # the last 4 numbers belong to the image name
+		vid = self.list_names[index][:len(self.list_names[index])-4] #  the others belong to the name of video
 
-        rgb_ima = os.path.join(self.path_images,vid, ima_name)
-        image = cv2.imread(rgb_ima)
-        ima_name = self.list_names[index][-4:]+'.png'
-        saliency = cv2.imread(os.path.join(self.path_saliency, vid,'GT','map', ima_name), 0)
+		rgb_ima = os.path.join(self.path_images,vid, ima_name)
+		image = cv2.imread(rgb_ima)
+		ima_name = self.list_names[index][-4:]+'.png'
+		saliency = cv2.imread(os.path.join(self.path_saliency, vid,'GT','map', str(int(ima_name.split('.')[0])) + '.png'), 0)
+		
+		# apply transformation
+		if self.transformation is not None:
+			# reshape
+			image = cv2.resize(image, (self.size[1], self.size[0]), interpolation=cv2.INTER_AREA)
+			saliency = cv2.resize(saliency, (self.size[1], self.size[0]), interpolation=cv2.INTER_AREA)
 
-        # apply transformation
-        if self.transformation is not None:
-            # reshape
-            image = cv2.resize(image, (self.size[1], self.size[0]), interpolation=cv2.INTER_AREA)
-            saliency = cv2.resize(saliency, (self.size[1], self.size[0]), interpolation=cv2.INTER_AREA)
+			# convert to foat
+			image = image.astype(np.float32)
+			saliency = saliency.astype(np.float32)
 
-            # convert to foat
-            image = image.astype(np.float32)
-            saliency = saliency.astype(np.float32)
+			# remove mean value
+			image -= self.mean
 
-            # remove mean value
-            image -= self.mean
+			# convert to torch Tensor
+			image = torch.FloatTensor(image)
 
-            # convert to torch Tensor
-            image = torch.FloatTensor(image)
-
-            # swap channel dimensions
-            image = image.permute(2,0,1)
+			# swap channel dimensions
+			image = image.permute(2,0,1)
 
 
-        if self.return_path:
-            return image, saliency, rgb_ima
-        else:
-            return image, saliency
+		if self.return_path:
+			return image, saliency, rgb_ima
+		else:
+			return image, saliency
--- src/salgan_ledov/train_bce.py
+++ src/salgan_ledov/train_bce.py
@@ -1,9 +1,10 @@
 import os
 
-from ..dataloader.datasetDHF1K import DHF1K
+from dataloader.datasetLEDOV import LEDOV
 from torch.utils.data import DataLoader
-from ..utils.salgan_utils import save_model, get_lr_optimizer, sendTelegram
-from ..utils.salgan_utils import salgan_generator
+from utils.salgan_utils import save_model, get_lr_optimizer
+from utils.sendTelegram import send
+from utils.salgan_generator import create_model
 
 import numpy as np
 
@@ -16,7 +17,7 @@ from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 
 from IPython import embed
-from tensorboard_logger import configure, log_value
+from tensorboard_logger import configure, log_value, log_histogram
 
 
 TRAIN = 'train'
@@ -24,127 +25,127 @@ VAL = 'val'
 TEST = 'test'
 
 def train_eval(mode, model, optimizer, dataloader):
-    if mode == TRAIN:
-        N = len(ds_train)/batch_size
-        model.train()
-    else:
-        N = len(ds_validate)/batch_size
-        model.eval()
+	if mode == TRAIN:
+		N = len(ds_train)/batch_size
+		model.train()
+	else:
+		N = len(ds_validate)/batch_size
+		model.eval()
 
-    total_loss = []
-    #iterate epoch...
-    for i, X in enumerate(dataloader[mode]):
-        inputs = X[0].cuda()
-        # noramlize saliency maps values between [0,1]
-        gt_maps = X[1].cuda()/255
+	total_loss = []
+	#iterate epoch...
+	for i, X in enumerate(dataloader[mode]):
+		inputs = X[0].cuda()
+		# noramlize saliency maps values between [0,1]
+		gt_maps = X[1].cuda()/255
 
-        predictions = model.forward(inputs).squeeze()
+		predictions = model.forward(inputs).squeeze()
 
-        # reduce size for loss
-        reduce_size = AvgPool2d((4,4))
-        pred_ = reduce_size(predictions)
-        gt_maps_ = reduce_size(gt_maps)
+		# reduce size for loss
+		reduce_size = AvgPool2d((4,4))
+		pred_ = reduce_size(predictions)
+		gt_maps_ = reduce_size(gt_maps)
 
-        pred_ = pred_.view(pred_.size()[0], -1)
-        gt_maps_ = gt_maps_.view(gt_maps_.size()[0], -1)
+		pred_ = pred_.view(pred_.size()[0], -1)
+		gt_maps_ = gt_maps_.view(gt_maps_.size()[0], -1)
 
-        loss = bce_loss(pred_, gt_maps_)
+		loss = bce_loss(pred_, gt_maps_)
 
-        # make actual step update
-        if mode==TRAIN:
-            # compute gradients
-            loss.backward()
-            # step optimizer
-            optimizer.step()
-            # reset grads for next step
-            optimizer.zero_grad()
+		# make actual step update
+		if mode==TRAIN:
+			# compute gradients
+			loss.backward()
+			# step optimizer
+			optimizer.step()
+			# reset grads for next step
+			optimizer.zero_grad()
 
 
-        print("\t{}/{} loss:{}".format(i, int(N), loss.item()))
-        total_loss.append(loss.item())
+		print("\t{}/{} loss:{}".format(i, int(N), loss.item()))
+		total_loss.append(loss.item())
 
-    total_loss=np.mean(total_loss)
+	total_loss=np.mean(total_loss)
 
-    return total_loss
+	return total_loss
 
 
 
 if __name__ == '__main__':
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--path_out", default='../trained_models/salgan_ledov',
-                type=str,
-                help="""set output path for the trained model""")
-    parser.add_argument("--batch_size", default=15,
-                type=int,
-                help="""Set batch size""")
-    parser.add_argument("--n_epochs", default=200, type=int,
-                help="""Set total number of epochs""")
-    parser.add_argument("--lr", type=float, default=0.01,
-                help="""Learning rate for training""")
-    parser.add_argument("--patience", type=int, default=10,
-                help="""Patience for learning rate scheduler (default 10)""")
-    args = parser.parse_args()
+	import argparse
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--path_out", default='../trained_models/salgan_ledov',
+				type=str,
+				help="""set output path for the trained model""")
+	parser.add_argument("--batch_size", default=10,
+				type=int,
+				help="""Set batch size""")
+	parser.add_argument("--n_epochs", default=200, type=int,
+				help="""Set total number of epochs""")
+	parser.add_argument("--lr", type=float, default=0.01,
+				help="""Learning rate for training""")
+	parser.add_argument("--patience", type=int, default=4,
+				help="""Patience for learning rate scheduler (default 10)""")
+	args = parser.parse_args()
 
 
-    # set output path ==========================================================
-    path_out = args.path_out
+	# set output path ==========================================================
+	path_out = args.path_out
 
-    if not os.path.exists(path_out):
-        # create output path
-        os.makedirs(path_out)
+	if not os.path.exists(path_out):
+		# create output path
+		os.makedirs(path_out)
 
-        # create output for models
-        path_models = os.path.join(path_out, 'models')
-        if not os.path.exists(path_models):
-            os.makedirs(path_models)
+	# create output for models
+	path_models = os.path.join(path_out, 'models')
+	if not os.path.exists(path_models):
+		os.makedirs(path_models)
 
-    # tensorboard
-    configure("{}".format(path_out), flush_secs=5)
+	# tensorboard
+	configure("{}".format(path_out), flush_secs=5)
 
-    # data =====================================================================
-    batch_size = args.batch_size
-    n_epochs = args.n_epochs
+	# data =====================================================================
+	batch_size = args.batch_size
+	n_epochs = args.n_epochs
 
-    # Datasets for DHF1K
-    ds_train = DHF1K(mode=TRAIN)
-    ds_validate = DHF1K(mode=VAL)
+	# Datasets for LEDOV
+	ds_train = LEDOV(mode=TRAIN,transformation=True)
+	ds_validate = LEDOV(mode=VAL,transformation=True)
 
-    # Dataloaders
-    dataloader = {
-        TRAIN: DataLoader(ds_train, batch_size=batch_size,
-                                shuffle=True, num_workers=4),
-        VAL: DataLoader(ds_validate, batch_size=batch_size,
-                                shuffle=False, num_workers=4)
-    }
-
-
+	# Dataloaders
+	dataloader = {
+		TRAIN: DataLoader(ds_train, batch_size=batch_size,
+								shuffle=True, num_workers=2),
+		VAL: DataLoader(ds_validate, batch_size=batch_size,
+								shuffle=False, num_workers=2)
+	}
 
 
-    # model ====================================================================
-    print("Init model...")
-    # init model with pre-trained weights
-    model = salgan_generator.create_model()
-    model.load_state_dict(torch.load('../trained_models/salgan_bce/models/best.pt'))
-    model.train()
-    model.cuda()
+
+
+	# model ====================================================================
+	print("Init model...")
+	# init model with pre-trained weights
+	model = create_model()
+	model.load_state_dict(torch.load('../trained_models/baseline_weights/gen_model.pt'))
+	model.train()
+	model.cuda()
 	cudnn.benchmark = True
 
-    # loss =====================================================================
-    print("BCE criterium...")
-    bce_loss = BCELoss()
+	# loss =====================================================================
+	print("BCE criterium...")
+	bce_loss = BCELoss()
 
-    # select only decoder parameters, keep vgg16 with pretrained weights
-    decoder_parameters = []
-    for i, (a, p) in enumerate(model.named_parameters()):
-        if i>25:
-            print(i, a, p.shape)
-            decoder_parameters.append(p)
+	# select only decoder parameters, keep vgg16 with pretrained weights
+	decoder_parameters = []
+	for i, (a, p) in enumerate(model.named_parameters()):
+		if i>25:
+			print(i, a, p.shape)
+			decoder_parameters.append(p)
 
-    optimizer = SGD(decoder_parameters,
-                    lr = args.lr, momentum=0.9, weight_decay=0.00001)
+	optimizer = SGD(decoder_parameters,
+					lr = args.lr, momentum=0.9, weight_decay=0.00001)
 
-    # set learning rate scheduler
+	# set learning rate scheduler
 	# ReduceLROnPlateau(
 		# optimizer,
 		# mode (str) 'min':lr es reduira quan la metrica no es redueixi mes,  'max' al contrari,
@@ -152,57 +153,57 @@ if __name__ == '__main__':
 		# patience (int) num epochs sense millora a partir dels quals es redueix lr,
 		# verbose (bool),
 	# )
-    scheduler = ReduceLROnPlateau(optimizer,
+	scheduler = ReduceLROnPlateau(optimizer,
 								'min',
 								patience=args.patience,
-                                verbose=True)
+								verbose=True)
 
-    best_loss=9999999
+	best_loss=9999999
 
-    # main loop training =======================================================
-    for id_epoch in range(n_epochs):
-        for mode in [VAL, TRAIN]:
-            # select dataloader
-            data_iterator = dataloader[mode]
+	# main loop training =======================================================
+	for id_epoch in range(n_epochs):
+		for mode in [VAL, TRAIN]:
+			# select dataloader
+			data_iterator = dataloader[mode]
 
-            # compute saliency metrics
-            # (not sure if this metrics are right thought)
-            if mode ==VAL:
-                print("Evaluating metrics....")
-                # only do 100 images from validation (too slow if not)
-                metrics = get_saliency_metrics(data_iterator, model, N=100)
+			# compute saliency metrics
+			# (not sure if this metrics are right thought)
+			# if mode ==VAL:
+			# 	print("Evaluating metrics....")
+			# 	# only do 100 images from validation (too slow if not)
+			# 	metrics = get_saliency_metrics(data_iterator, model, N=100)
 
-                # log metric values
-                for metric in metrics.keys():
-                    log_value("Metrics/{}".format(metric),
-                                metrics[metric], id_epoch)
+			# 	# log metric values
+			# 	for metric in metrics.keys():
+			# 		log_value("Metrics/{}".format(metric),
+			# 					metrics[metric], id_epoch)
 
-            # get epoch loss
-            print("{} epoch {}".format(mode, id_epoch))
+			# get epoch loss
+			print("{} epoch {}".format(mode, id_epoch))
 
-            print("-----------")
+			print("-----------")
 
-            epoch_loss = train_eval(mode, model, optimizer, dataloader)
+			epoch_loss = train_eval(mode, model, optimizer, dataloader)
 
-            lr = list(get_lr_optimizer(optimizer))[0]
-            print("-----------")
-            print("Done! {} epoch {} loss {}".format(mode, id_epoch, epoch_loss))
-            send("{} epoch {}/{} loss {}".format(mode, id_epoch, n_epochs, epoch_loss))
-            print("\n")
+			lr = list(get_lr_optimizer(optimizer))[0]
+			print("-----------")
+			print("Done! {} epoch {} loss {}".format(mode, id_epoch, epoch_loss))
+			send("{} epoch {}/{} loss {}".format(mode, id_epoch, n_epochs, epoch_loss))
+			print("\n")
 
-            # record loss
-            log_value("loss/{}".format(mode), epoch_loss, id_epoch)
-            log_value("lr/{}".format(mode), lr, id_epoch)
+			# record loss
+			log_value("loss/{}".format(mode), epoch_loss, id_epoch)
+			log_value("lr/{}".format(mode), lr, id_epoch)
 			for v in model.state_dict():
-    			log_histogram("Layer {}".format(v), model.state_dict()[v], id_epoch)
+				log_histogram("Layer {}".format(v), model.state_dict()[v], id_epoch)
 
-            save_model(model, optimizer, id_epoch, path_out, name_model='{:03d}'.format(id_epoch))
-            scheduler.step(epoch_loss)
-            # store model if val loss improves
-            if mode==VAL:
-                if best_loss > epoch_loss:
-                    # update loss
-                    best_loss = epoch_loss
+			# save_model(model, optimizer, id_epoch, path_out, name_model='{:03d}'.format(id_epoch))
+			scheduler.step(epoch_loss)
+			# store model if val loss improves
+			if mode==VAL:
+				if best_loss > epoch_loss:
+					# update loss
+					best_loss = epoch_loss
 
-                    save_model(model, optimizer, id_epoch, path_out, name_model='best1')
-                    # scheduler.step(epoch_loss)
+					save_model(model, optimizer, id_epoch, path_out, name_model='best')
+					# scheduler.step(epoch_loss)
