bb1a3a69ec20e4983b53d03e631b3c0f9b1de871
JuanJo Nieto <juanjo.3ns@gmail.com>
2018-12-24 18:10:11 +0000

    experiment low LR and adam optimizer

--- src/salgan_dhf1k/train_bce.py
+++ src/salgan_dhf1k/train_bce.py
@@ -4,7 +4,7 @@ from dataloader.datasetDHF1K import DHF1K
 from torch.utils.data import DataLoader
 from utils.salgan_utils import save_model, get_lr_optimizer
 from utils.sendTelegram import send
-from utils.salgan_generator_bn import create_model, add_bn
+from utils.salgan_generator import create_model, add_bn
 
 import numpy as np
 
@@ -72,7 +72,7 @@ def train_eval(mode, model, optimizer, dataloader):
 if __name__ == '__main__':
 	import argparse
 	parser = argparse.ArgumentParser()
-	parser.add_argument("--path_out", default='../trained_models/salcoord_dhf1k_batchnorm1',
+	parser.add_argument("--path_out", default='../trained_models/salgan_dhf1k_adamoptim',
 				type=str,
 				help="""set output path for the trained model""")
 	parser.add_argument("--batch_size", default=12,
@@ -88,7 +88,7 @@ if __name__ == '__main__':
 				help="""Enable coordconv""")
 	parser.add_argument("--lr", type=float, default=0.0001,
 				help="""Learning rate for training""")
-	parser.add_argument("--patience", type=int, default=2,
+	parser.add_argument("--patience", type=int, default=3,
 				help="""Patience for learning rate scheduler (default 10)""")
 	args = parser.parse_args()
 
@@ -133,7 +133,7 @@ if __name__ == '__main__':
 	# model ====================================================================
 	print("Init model...")
 	# init model with pre-trained weights
-	vgg_weights = torch.load('../trained_models/salcoord_dhf1k_batchnorm/models/best.pt')['state_dict']
+	vgg_weights = torch.load('../trained_models/salgan_salicon_daugmfromscr3/models/best.pt')['state_dict']
 	if DEPTH:
 		model = create_model(4)
 		# Mean of RGB weights of first layer with size [64,1,3,3]
@@ -154,7 +154,7 @@ if __name__ == '__main__':
 	model.load_state_dict(vgg_weights)
 
 	# Add batch normalization to current model
-	# model = add_bn(model)
+	model = add_bn(model)
 
 	model.train()
 	model.cuda()
@@ -182,11 +182,17 @@ if __name__ == '__main__':
 			decoder_parameters.append(p)
 		else: base_params.append(p)
 
-	optimizer = SGD(model.parameters(),
-					lr = 0.00001,
-					momentum=0.9,
-					weight_decay=0.00001,
-					nesterov=True)
+	# ADAM OPTIMIZER
+	optimizer = Adam(model.parameters(),
+					lr = 0.000001,
+					weight_decay=0.0001)
+
+	# STOCHASTIC GRADIENT DESCENT
+	# optimizer = SGD(model.parameters(),
+	# 				lr = 0.00001,
+	# 				momentum=0.9,
+	# 				weight_decay=0.00001,
+	# 				nesterov=True)
 
 	# set learning rate scheduler
 	# ReduceLROnPlateau(
--- src/utils/sendTelegram.py
+++ src/utils/sendTelegram.py
@@ -1,6 +1,8 @@
 import requests
 import json
-token = ''
+import os
+# token = os.environ['TG_KEY']
+token = '658824281:AAETui7gl4muFLRsod1j2cGnuIDox_hj6hY'
 url = 'https://api.telegram.org/bot' + token+ '/sendMessage'
 def send(message):
     r = requests.post(
